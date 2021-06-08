7e072e30cc9a4852570d38df802fc5ac64a427b6
Wil McCauley <wmccauley724@gmail.com>
2021-06-08 08:26:04 -0600

    Run migration, TDD tea and customer model generation

---
+++ spec/factories/customers.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :customer do
+    first_name { "MyString" }
+    last_name { "MyString" }
+    address { "MyString" }
+  end
+end
---
+++ spec/factories/teas.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :tea do
+    title { "MyString" }
+    temp_c { 1 }
+    brew_for { 1.5 }
+  end
+end

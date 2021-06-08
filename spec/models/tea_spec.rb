a442afdcd4f2756b2a8956c73d38b4258fd0bbf9
Wil McCauley <wmccauley724@gmail.com>
2021-06-08 07:50:26 -0600

    Begin model TDD process

---
+++ spec/models/tea_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Tea, type: :model do
+  # describe 'validations' do
+    # it { should validate_presence_of  }
+  # end
+  describe 'relationships' do
+    it { should have_many :subscriptions }
+    it { should have_many(:customers).through(:subscriptions) }
+  end
+end

4d1daea703c3ebc4f42215f8dfb68cbf2b8fd255
Wil McCauley <wmccauley724@gmail.com>
2021-06-08 10:40:16 -0600

    Add faker data for tea factories

--- spec/factories/teas.rb
+++ spec/factories/teas.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :tea do
-    title { "MyString" }
-    temp_c { 1 }
-    brew_for { 1.5 }
+    title { Faker::Tea.variety }
+    temp_c { 100 }
+    brew_for { ([1, 2, 3, 4].sample) * 0.5 }
   end
 end

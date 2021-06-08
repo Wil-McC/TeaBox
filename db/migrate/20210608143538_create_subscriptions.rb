ffa9ce178863d423a1428d3318e1609c7db04566
Wil McCauley <wmccauley724@gmail.com>
2021-06-08 08:40:12 -0600

    Run migrations

--- db/migrate/20210608143538_create_subscriptions.rb
+++ db/migrate/20210608143538_create_subscriptions.rb
@@ -1,8 +1,8 @@
 class CreateSubscriptions < ActiveRecord::Migration[5.2]
   def change
     create_table :subscriptions do |t|
-      t.references :customer_id, foreign_key: true
-      t.references :tea_id, foreign_key: true
+      t.references :customer, foreign_key: true
+      t.references :tea, foreign_key: true
       t.string :status
       t.integer :frequency
       t.float :pricepoint
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe , type: :model do
+RSpec.describe Customer, type: :model do
   # describe 'validations' do
     # it { should validate_presence_of  }
   # end
